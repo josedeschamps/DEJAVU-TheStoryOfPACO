d73ac3bfb11d20b5c11671c0efbff9d94336e3f9
Jose Deschamps <deschamps.jose001@gmail.com>
2017-10-10 08:06:34 -0400

    still playing around with ideas

---
+++ The Room Within/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed;
+	private Rigidbody2D rb2d;
+
+
+	// Use this for initialization
+	void Start () {
+
+		rb2d = GetComponent<Rigidbody2D> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		float Xpos = Input.GetAxis ("Horizontal");
+		rb2d.velocity = new Vector2 (Xpos * moveSpeed, rb2d.velocity.y);
+		
+	}
+}
